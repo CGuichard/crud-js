eb70b55a30467f80b6c0cd2e4a60768144fe8362
Clément GUICHARD <clement.guichard0@gmail.com>
2020-03-03 13:18:59 +0100

    Refactoring folder structure, and updating of static resources routes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CrudJS
 
 CrudJS is a web component whose purpose is to have a front-end crud to manipulate your data and communicate it with your server back-end.
+
+[View demo](/demo)
--- index.js
+++ index.js
@@ -6,19 +6,28 @@ var marked = require('marked');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use("/static", express.static(path.join(__dirname + '/static/node_modules/')));
+app.use("/static", express.static(path.join(__dirname + '/static/')));
+app.use("/dependencies", express.static(path.join(__dirname + '/static/node_modules/')));
 app.use("/css", express.static(path.join(__dirname + '/static/css/')));
 app.use("/js", express.static(path.join(__dirname + '/static/js/')));
-app.use("/images", express.static(path.join(__dirname + '/images/')));
+app.use("/images", express.static(path.join(__dirname + '/static/images/')));
 
 app.get('/', function(req, res) {
+    res.redirect('/readme');
+});
+
+app.get('/readme', function(req, res) {
     var path = __dirname + '/README.md';
     var file = fs.readFileSync(path, 'utf8');
-    res.send(marked(file.toString()));
+    res.render('readme.ejs', {readme_html: marked(file.toString())});
 });
 
 app.get('/demo', function(req, res) {
     res.render('demo.ejs');
+});
+
+app.get('/docs', function(req, res) {
+    res.send('Docs');
 });
 
 var port = 5000;
